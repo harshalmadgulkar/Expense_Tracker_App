9c9dc2e5ad6b5acfe403dcd02ca4fb6e426b907b
harshalmadgulkar <harshal.madgulkar725@gmail.com>
2024-03-04 08:26:44 +0530

    Update firebaseInit.js

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAuDBdmUoGZguzZHgHdBGILPV-LzxbiIYU",
